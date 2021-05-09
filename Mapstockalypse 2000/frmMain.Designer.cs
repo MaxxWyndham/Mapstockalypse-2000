2a17b550220e730cd36e7e415cafc33b138bb1fb
Maxx Wyndham <maxx@maxxwyndham.com>
2021-05-09 12:12:19 +0100

    Add project files.

---
+++ Mapstockalypse 2000/frmMain.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Mapstockalypse_2000
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbPath = new System.Windows.Forms.GroupBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.scPanels = new System.Windows.Forms.SplitContainer();
+            this.lstRaces = new System.Windows.Forms.ListBox();
+            this.scRight = new System.Windows.Forms.SplitContainer();
+            this.lstMods = new System.Windows.Forms.ListBox();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.fbdBrowse = new System.Windows.Forms.FolderBrowserDialog();
+            this.llToxicRagers = new System.Windows.Forms.LinkLabel();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.gbPath.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scPanels)).BeginInit();
+            this.scPanels.Panel1.SuspendLayout();
+            this.scPanels.Panel2.SuspendLayout();
+            this.scPanels.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scRight)).BeginInit();
+            this.scRight.Panel1.SuspendLayout();
+            this.scRight.Panel2.SuspendLayout();
+            this.scRight.SuspendLayout();
+            this.pnlButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbPath
+            // 
+            this.gbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbPath.Controls.Add(this.lblPath);
+            this.gbPath.Controls.Add(this.btnBrowse);
+            this.gbPath.Controls.Add(this.txtPath);
+            this.gbPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gbPath.Location = new System.Drawing.Point(12, 12);
+            this.gbPath.Name = "gbPath";
+            this.gbPath.Size = new System.Drawing.Size(534, 42);
+            this.gbPath.TabIndex = 0;
+            this.gbPath.TabStop = false;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(6, 17);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(106, 13);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "Carmageddon 2 path";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(453, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(118, 14);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(329, 20);
+            this.txtPath.TabIndex = 3;
+            this.txtPath.Text = "D:\\Carmageddon Installations\\Carmageddon 2";
+            // 
+            // scPanels
+            // 
+            this.scPanels.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scPanels.Location = new System.Drawing.Point(12, 60);
+            this.scPanels.Name = "scPanels";
+            // 
+            // scPanels.Panel1
+            // 
+            this.scPanels.Panel1.Controls.Add(this.lstRaces);
+            // 
+            // scPanels.Panel2
+            // 
+            this.scPanels.Panel2.Controls.Add(this.scRight);
+            this.scPanels.Size = new System.Drawing.Size(534, 398);
+            this.scPanels.SplitterDistance = 247;
+            this.scPanels.TabIndex = 1;
+            // 
+            // lstRaces
+            // 
+            this.lstRaces.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstRaces.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstRaces.FormattingEnabled = true;
+            this.lstRaces.IntegralHeight = false;
+            this.lstRaces.Location = new System.Drawing.Point(0, 0);
+            this.lstRaces.Name = "lstRaces";
+            this.lstRaces.Size = new System.Drawing.Size(247, 398);
+            this.lstRaces.TabIndex = 0;
+            this.lstRaces.SelectedIndexChanged += new System.EventHandler(this.lstRaces_SelectedIndexChanged);
+            // 
+            // scRight
+            // 
+            this.scRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scRight.Location = new System.Drawing.Point(0, 0);
+            this.scRight.Name = "scRight";
+            this.scRight.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // scRight.Panel1
+            // 
+            this.scRight.Panel1.Controls.Add(this.lstMods);
+            // 
+            // scRight.Panel2
+            // 
+            this.scRight.Panel2.Controls.Add(this.pnlButtons);
+            this.scRight.Size = new System.Drawing.Size(283, 398);
+            this.scRight.SplitterDistance = 299;
+            this.scRight.TabIndex = 0;
+            // 
+            // lstMods
+            // 
+            this.lstMods.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstMods.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstMods.FormattingEnabled = true;
+            this.lstMods.IntegralHeight = false;
+            this.lstMods.Location = new System.Drawing.Point(0, 0);
+            this.lstMods.Name = "lstMods";
+            this.lstMods.Size = new System.Drawing.Size(283, 299);
+            this.lstMods.TabIndex = 0;
+            this.lstMods.SelectedIndexChanged += new System.EventHandler(this.lstMods_SelectedIndexChanged);
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlButtons.Controls.Add(this.llToxicRagers);
+            this.pnlButtons.Controls.Add(this.btnInstall);
+            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlButtons.Location = new System.Drawing.Point(0, 0);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(283, 95);
+            this.pnlButtons.TabIndex = 0;
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInstall.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnInstall.Location = new System.Drawing.Point(3, 3);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(275, 41);
+            this.btnInstall.TabIndex = 1;
+            this.btnInstall.Text = "replace {race} with {mod}";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // fbdBrowse
+            // 
+            this.fbdBrowse.ShowNewFolderButton = false;
+            // 
+            // llToxicRagers
+            // 
+            this.llToxicRagers.Location = new System.Drawing.Point(3, 47);
+            this.llToxicRagers.Name = "llToxicRagers";
+            this.llToxicRagers.Size = new System.Drawing.Size(275, 41);
+            this.llToxicRagers.TabIndex = 2;
+            this.llToxicRagers.TabStop = true;
+            this.llToxicRagers.Text = "Built with love by the one and only Toxic Ranger";
+            this.llToxicRagers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.llToxicRagers.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llToxicRagers_LinkClicked);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblVersion.Location = new System.Drawing.Point(508, 453);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(48, 15);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "v1.0.0.0";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 470);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.scPanels);
+            this.Controls.Add(this.gbPath);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Mapstockalypse 2000";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.gbPath.ResumeLayout(false);
+            this.gbPath.PerformLayout();
+            this.scPanels.Panel1.ResumeLayout(false);
+            this.scPanels.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.scPanels)).EndInit();
+            this.scPanels.ResumeLayout(false);
+            this.scRight.Panel1.ResumeLayout(false);
+            this.scRight.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.scRight)).EndInit();
+            this.scRight.ResumeLayout(false);
+            this.pnlButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.SplitContainer scPanels;
+        private System.Windows.Forms.ListBox lstRaces;
+        private System.Windows.Forms.SplitContainer scRight;
+        private System.Windows.Forms.ListBox lstMods;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Button btnInstall;
+        private System.Windows.Forms.LinkLabel llToxicRagers;
+        private System.Windows.Forms.FolderBrowserDialog fbdBrowse;
+        private System.Windows.Forms.Label lblVersion;
+    }
+}
+
---
+++ Mapstockalypse 2000/frmMain.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows.Forms;
+
+using ToxicRagers.Carmageddon.Helpers;
+using ToxicRagers.Carmageddon2.Formats;
+
+namespace Mapstockalypse_2000
+{
+    public partial class frmMain : Form
+    {
+        readonly List<string> tagLines = new List<string>
+        {
+            "Toxic Ragers?  Never heard of 'em",
+            "Built with love by the one and only Toxic Ranger",
+            "I bet you don't have the guts to click this",
+            "Elephants?"
+        };
+
+        string currentRoot;
+        string race;
+        string mod;
+        RacesTXT races;
+        readonly List<RaceDetails> mods = new List<RaceDetails>();
+        readonly Dictionary<int, string> modZips = new Dictionary<int, string>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = $"v{Application.ProductVersion}";
+
+            reloadUI(Directory.GetCurrentDirectory());
+        }
+
+        private void reloadUI(string root)
+        {
+            currentRoot = root;
+
+            mods.Clear();
+            modZips.Clear();
+            lstRaces.Items.Clear();
+            lstMods.Items.Clear();
+            llToxicRagers.Text = tagLines[new Random().Next(tagLines.Count)];
+            txtPath.Text = root;
+            btnInstall.Text = "select a mod and a map to replace, then click here!";
+            btnInstall.Enabled = false;
+
+            string twtFile = Path.Combine(txtPath.Text, "data.twt");
+
+            if (File.Exists(twtFile))
+            {
+                TWT twt = TWT.Load(twtFile);
+
+                foreach (TWTEntry entry in twt.Contents)
+                {
+                    twt.Extract(entry, Path.Combine(txtPath.Text, "data"));
+                }
+
+                File.Move(twtFile, Path.Combine(txtPath.Text, "data.twat"));
+            }
+
+            string racesTXT = Path.Combine(txtPath.Text, "data", "races.txt");
+
+            if (File.Exists(racesTXT))
+            {
+                races = RacesTXT.Load(Path.Combine(txtPath.Text, "data", "races.txt"));
+
+                foreach (RaceDetails race in races.Races)
+                {
+                    if (!race.BoundaryRace) { lstRaces.Items.Add(race.Name); }
+                }
+            }
+
+            string mapModDir = Path.Combine(txtPath.Text, ".mods", "maps");
+
+            if (Directory.Exists(mapModDir))
+            {
+                foreach (string file in Directory.GetFiles(mapModDir, "*.zip"))
+                {
+                    using (FileStream fs = new FileStream(file, FileMode.Open))
+                    using (ZipArchive archive = new ZipArchive(fs, ZipArchiveMode.Read))
+                    {
+                        if (archive.Entries.Any(f => f.Name.EndsWith(".c2t", StringComparison.InvariantCultureIgnoreCase)) &&
+                            archive.Entries.Any(f => f.Name == "" && f.FullName.ToLower() == @"data/races/"))
+                        {
+                            ZipArchiveEntry entry = archive.Entries.First(f => f.Name.EndsWith(".c2t", StringComparison.InvariantCultureIgnoreCase));
+
+                            using (MemoryStream ms = new MemoryStream())
+                            using (BinaryReader br = new BinaryReader(ms))
+                            {
+                                entry.Open().CopyTo(ms);
+
+                                ms.Seek(0, SeekOrigin.Begin);
+
+                                RaceDetails details = RaceDetails.Load(new DocumentParser(br));
+
+                                int index = lstMods.Items.Add(details.Name);
+
+                                modZips.Add(index, file);
+                                mods.Add(details);
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (string file in Directory.GetFiles(txtPath.Text, "*.c2t"))
+            {
+                RaceDetails details = RaceDetails.Load(new DocumentParser(file));
+
+                if (!mods.Any(m => m.Name == details.Name))
+                {
+                    lstMods.Items.Add(details.Name);
+                    mods.Add(details);
+                }
+            }
+        }
+
+        private void updateAction()
+        {
+            btnInstall.Text = $"replace {race} with {mod}";
+
+            if (!string.IsNullOrEmpty(race) && !string.IsNullOrEmpty(mod))
+            {
+                btnInstall.Enabled = true;
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (fbdBrowse.ShowDialog() == DialogResult.OK && Directory.Exists(fbdBrowse.SelectedPath))
+            {
+                reloadUI(fbdBrowse.SelectedPath);
+            }
+        }
+
+        private void lstRaces_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            race = lstRaces.GetItemText(lstRaces.SelectedItem);
+
+            updateAction();
+        }
+
+        private void lstMods_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mod = lstMods.GetItemText(lstMods.SelectedItem);
+
+            updateAction();
+        }
+
+        private void llToxicRagers_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            llToxicRagers.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://www.toxic-ragers.co.uk");
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            bool success = false;
+
+            RaceDetails oldRace = races.Races[lstRaces.SelectedIndex];
+            RaceDetails newRace = mods[lstMods.SelectedIndex];
+
+            string oldRaceName = Path.GetFileNameWithoutExtension(oldRace.RaceFilename);
+
+            // extract archive (if installing from one)
+            if (modZips.ContainsKey(lstMods.SelectedIndex))
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(modZips[lstMods.SelectedIndex]))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        if (entry.Name == "") { continue; }
+
+                        string destinationPath = Path.GetFullPath(Path.Combine(txtPath.Text, entry.FullName));
+
+                        if (destinationPath.StartsWith(txtPath.Text, StringComparison.Ordinal)) 
+                        {
+                            if (File.Exists(destinationPath)) { File.Delete(destinationPath); }
+
+                            entry.ExtractToFile(destinationPath); 
+                        }
+                    }
+                }
+            }
+
+            // validate installation by checking MapFilename exists
+            if (Directory.Exists(Path.Combine(txtPath.Text, "data", "races", oldRaceName)))
+            {
+                success = File.Exists(Path.Combine(txtPath.Text, "data", "races", oldRaceName, $"{oldRaceName}.txt"));
+            }
+            else
+            {
+                if (File.Exists(Path.Combine(txtPath.Text, "data", "races", $"{oldRaceName}.twt")))
+                {
+                    TWT twt = TWT.Load(Path.Combine(txtPath.Text, "data", "races", $"{oldRaceName}.twt"));
+
+                    success = twt.Contents.Any(entry => string.Compare(entry.Name, $"{oldRaceName}.txt", true) == 0);
+                }
+            }
+
+            if (success)
+            {
+                // create c2t file for track to be replaced
+                using (DocumentWriter dw = new DocumentWriter(Path.Combine(txtPath.Text, $"{oldRaceName}.c2t")))
+                {
+                    oldRace.Write(dw);
+                }
+
+                // update races.txt
+                races.Races[lstRaces.SelectedIndex] = newRace;
+                File.Delete(Path.Combine(txtPath.Text, "data", "races.old"));
+                File.Move(Path.Combine(txtPath.Text, "data", "races.txt"), Path.Combine(txtPath.Text, "data", "races.old"));
+                races.Save(Path.Combine(txtPath.Text, "data", "races.txt"));
+
+                reloadUI(currentRoot);
+            }
+        }
+    }
+}
